8390c5cd3245dd4dbeda0505528804e338f9a6a3
Chukwuma Mary <petersbrenda47@gmail.com>
2023-05-05 13:22:02 -0400

    Added Form component

--- components/common/Form.tsx
+++ components/common/Form.tsx
@@ -1,26 +1,166 @@
-import React from 'react'
-import { Box, Typography, FormControl, Button, FormHelperText, TextField, TextareaAutosize, Stack, Select, MenuItem } from '@mui/material'
+import React from "react";
+import {
+  Box,
+  Typography,
+  FormControl,
+  Button,
+  FormHelperText,
+  TextField,
+  TextareaAutosize,
+  Stack,
+  Select,
+  MenuItem,
+} from "@mui/material";
 
-import { FormProps } from '@/interfaces/common'
-import CustomButton from './CustomButton'
+import { FormProps } from "@/interfaces/common";
+import CustomButton from "./CustomButton";
 
-const Form = ({type, register, handleSubmit, handleImageChange, formLoading, propertyImage} : FormProps) => {
+const Form = ({
+  type,
+  register,
+  handleSubmit,
+  handleImageChange,
+  formLoading,
+  propertyImage,
+}: FormProps) => {
   return (
     <Box>
-      <Typography fontSize={25} fontWeight={700}
-      color='#11142d'>
+      <Typography fontSize={25} fontWeight={700} color="#11142d">
         {type} a Property
       </Typography>
-      <Box  mt={2.5} borderRadius='15px' padding='20px' bgColor='#fcfcfc'>
-        <form style={{marginTop: '20px', width: '100%', display: 'flex', flexDirection: 'column', gap: '20px'}} onSubmit={handleSubmit}>
+      <Box mt={2.5} borderRadius="15px" padding="20px">
+        <form
+          style={{
+            marginTop: "20px",
+            width: "100%",
+            display: "flex",
+            flexDirection: "column",
+            gap: "20px",
+          }}
+          onSubmit={handleSubmit}
+        >
           <FormControl>
-            <FormHelperText sx={{fontWeight: 500, margin: '10px', fontSize: 16,color: '#11142d'}}>Enter Property Name</FormHelperText>
+            <FormHelperText
+              sx={{
+                fontWeight: 500,
+                margin: "10px",
+                fontSize: 16,
+                color: "#11142d",
+              }}
+            >
+              Enter Property Name
+            </FormHelperText>
+            <TextField
+              fullWidth
+              required
+              id="outlined-basic"
+              color="info"
+              variant="outlined"
+              {...register("title", { required: true })}
+            />
           </FormControl>
-        </form>
+          <FormControl>
+            <FormHelperText
+              sx={{
+                fontWeight: 500,
+                margin: "10px",
+                fontSize: 16,
+                color: "#11142d",
+              }}
+            >
+              Enter Description
+            </FormHelperText>
+            <TextareaAutosize
+              minRows={5}
+              required
+              placeholder="Write Description"
+              color="info"
+              style={{
+                width: "100%",
+                background: "transparent",
+                fontSize: "16px",
+                borderColor: "rgba(0,0,0,0.23)",
+                borderRadius: 6,
+                padding: 10,
+                color: "#919191",
+              }}
+              {...register("title", { required: true })}
+            />
+          </FormControl>
+          <Stack direction='row' gap={4}>
+            <FormControl sx={{flex:1}}>
+              <FormHelperText sx={{fontWeight: 500, fontSize: 16 , margin: '10px', color: '#11142d'}}>
+                Select Property Type
+              </FormHelperText>
+              <Select variant='outlined' color='info' displayEmpty required inputProps={{'aria-label': 'Without label'}} defaultValue='apartment' {...register('propertyType', {required: true})} >
+              <MenuItem value="apartment">Apartment</MenuItem>
+              <MenuItem value="villa">Villa</MenuItem>
+              <MenuItem value="farmhouse">Farmhouse</MenuItem>
+              <MenuItem value="condos">Condos</MenuItem>
+              <MenuItem value="townhouse">Townhouse</MenuItem>
+              <MenuItem value="duplex">Duplex</MenuItem>
+              <MenuItem value="studio">Studio</MenuItem>
+              <MenuItem value="chalet">Chalet</MenuItem>
+              </Select>
+            </FormControl>
+            <FormControl sx={{ flex: 1 }}>
+            <FormHelperText sx={{ fontWeight: 500, margin: '10px 0', fontSize: 16, color: '#11142D' }}>Enter property price</FormHelperText>
+            <TextField
+              fullWidth
+              required
+              id="outlined-basic"
+              variant="outlined"
+              color="info"
+              type="number"
+              {...register('price', { required: true })}
+            />
+          </FormControl>
+        </Stack>
 
+        <FormControl>
+          <FormHelperText sx={{ fontWeight: 500, margin: '10px 0', fontSize: 16, color: '#11142D' }}>Enter location</FormHelperText>
+          <TextField
+            fullWidth
+            required
+            id="outlined-basic"
+            variant="outlined"
+            color="info"
+            {...register('location', { required: true })}
+          />
+        </FormControl>
+
+        <Stack direction="column" gap={1} justifyContent="center" mb={2}>
+          <Stack direction="row" gap={2}>
+            <Typography color="#11142D" fontSize={16} fontWeight={500} my="10px">Property Photo</Typography>
+
+            <Button component="label" sx={{ width: 'fit-content', color: '#2ED480', textTransform: 'capitalize', fontSize: 16 }}>
+              Upload *
+              <input
+                hidden
+                accept="image/*"
+                type="file"
+                onChange={(e) => {
+                  // @ts-ignore
+                  handleImageChange(e.target.files[0]);
+                }}
+              />
+            </Button>
+          </Stack>
+          <Typography fontSize={14} color="#808191" sx={{ wordBreak: 'break-all' }}>
+            {propertyImage?.name}
+          </Typography>
+        </Stack>
+
+        <CustomButton
+          type="submit"
+          title={formLoading ? 'Submitting...' : 'Submit'}
+          backgroundColor="#475BE8"
+          color="#FCFCFC"
+        />
+        </form>
       </Box>
     </Box>
-  )
-}
+  );
+};
 
-export default Form
+export default Form;
