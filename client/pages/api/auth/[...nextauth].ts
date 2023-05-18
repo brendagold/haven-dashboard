3183a040b75cb77ac7ae88cebfe057a9935d96c6
Chukwuma Mary <petersbrenda47@gmail.com>
2023-05-18 03:17:54 -0400

    clean code base for nextauth

--- client/pages/api/auth/[...nextauth].ts
+++ client/pages/api/auth/[...nextauth].ts
@@ -1,12 +1,8 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import axios from "axios";
-import SignToken from "./../../../utils/signinToken";
-import {saveToLocalStorage, called} from "./../../../utils/localStorage";
-
 
 // Initialize NextAuth
-
 
 export default NextAuth({
   providers: [
@@ -20,14 +16,6 @@ export default NextAuth({
   callbacks: {
     async signIn({ user }) {
       try {
-        //   const userData = {
-        //   name: profile?.name,
-        //   email: profile?.email,
-        //   avatar: profile?.picture,
-        // };
-        //console.log(`This is user, ${user}`)
-        //console.log(profile)
-        //console.log(`This is account, ${account}`)
         const response = await axios.post(
           "http://localhost:8080/api/v1/users",
           user,
@@ -37,21 +25,10 @@ export default NextAuth({
             },
           }
         );
-        //const data = await response.json();
-         if (response.status === 200) {
-          user.userId = response.data._id 
-          user.allProperties = response.data.allProperties
-        // user = {...user, data}
-        //signIn.user = response.data
-        //   console.log(response.data);
-        //   saveToLocalStorage('user', response.data)
-        //   called("Hello, I am Brenda")
-        //   if (typeof window !== undefined){
-        //     console.log("Hello");
-        //     //localStorage.setItem("user", JSON.stringify(response.data))
-        //     }
+        if (response.status === 200) {
+          user.userId = response.data._id;
+          user.allProperties = response.data.allProperties;
           return true;
-          
         } else {
           return Promise.reject();
         }
@@ -60,33 +37,14 @@ export default NextAuth({
       }
     },
     async jwt({ token, user }) {
-  //    console.log(user)
-  //    if (user) {
-  //     token = { userId: user.userId, allProperties: user.allProperties }
-  // }
-  //     return {...token, ...user};
-  user && (token.user = user)
-        return token
+      user && (token.user = user);
+      return token;
     },
     async session({ session, token }) {
-      // session.userId = token.userId,
-      // session.allProperties = token.allProperties
-      //session.user = token as any;
-      // return session;
-      session.user = token.user
-        return session
+      session.user = token.user;
+      return session;
     },
   },
 });
 
-// async jwt({ token, user, account }) {
-//   if (account) {
-//     const userLoggedIn = await SignToken(user?.email as string);
-//     token.loggedUser = userLoggedIn;
-//   }
-//   return token;
-// },
-// async session({ session, token, user }) {
-//   session.loggedUser = token.loggedUser;
-//   return session;
-// },
+
--- client/utils/signinToken.js
+++
@@ -1,8 +0,0 @@
-import jwt from 'jsonwebtoken';
-
-const SignToken = async (email)=> {
-const token = await jwt.sign({id:email}, process.env.JWT_SECRET, {expiresIn: '1d'});
-    return token
-}
-
-export default SignToken;
