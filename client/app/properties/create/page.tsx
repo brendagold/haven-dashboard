adb716ee18f8f27f576794e725144a67606dac62
Chukwuma Mary <petersbrenda47@gmail.com>
2023-05-18 04:58:56 -0400

    added create property function to frontend

--- client/app/properties/create/page.tsx
+++ client/app/properties/create/page.tsx
@@ -1,25 +1,64 @@
-'use client'
+"use client";
 
-import React, {useState} from 'react'
-import { useRouter } from 'next/navigation';
-import { FieldValues, useForm } from 'react-hook-form'
-
-import Form from '@/components/common/Form';
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { FieldValues, useForm } from "react-hook-form";
+import { useSession} from "next-auth/react";
+import axios from "axios";
+import Form from "@/components/common/Form";
 
 const CreateProperty = () => {
-    const router = useRouter();
-    const [propertyImage, setPropertyImage] = useState({name: '', url: ''})
-    const [formLoading, setFormLoading] = useState(false)
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-      } = useForm();
+  const router = useRouter();
+  const {data: session} = useSession()
+  const user = session?.user
+  const [propertyImage, setPropertyImage] = useState({ name: "", url: "" });
+  const [formLoading, setFormLoading] = useState(false);
+  
+  const {
+    register,
+    formState: { errors },
+  } = useForm();
+ 
+ 
+  const handleImageChange = (file: File) => {
+    const reader = (readFile: File) =>
+      new Promise<string>((resolve, reject) => {
+        const fileReader = new FileReader();
+        fileReader.onload = () => resolve(fileReader.result as string);
+        fileReader.readAsDataURL(readFile);
+      });
 
-      const handleImageChange = () => {}
+    reader(file).then((result: string) =>
+      setPropertyImage({ name: file?.name, url: result })
+    );
+  };
+
+  // const onFinishHandler = async (data: FieldValues) => {
+  //   if (!propertyImage.name) return alert("Please upload a property image");
+
+  //   await onFinish({ ...data, photo: propertyImage.url, email: user.email });
+
+  //   navigate("/properties");
+  // };
+
+  const handleSubmit = async (data: FieldValues, e: any) => {
+    e.preventDefault()
+    if (!propertyImage.name) return alert("Please upload a property image");
+    await axios.post(
+      "http://localhost:8080/api/v1/properties",
+      { ...data, photo: propertyImage.url, email: user?.email })
+      router.push('/properties')
+  } 
   return (
-    <Form type="Create" register={register} formLoading={formLoading} handleSubmit={handleSubmit} handleImageChange={handleImageChange} propertyImage={propertyImage} />
-  )
-}
+    <Form
+      type="Create"
+      register={register}
+      formLoading={formLoading}
+      handleSubmit={handleSubmit}
+      handleImageChange={handleImageChange}
+      propertyImage={propertyImage}
+    />
+  );
+};
 
-export default CreateProperty
+export default CreateProperty;
